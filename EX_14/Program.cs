6bb50351ae5fc3c9952bedbc50f48873b29645c6
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 21:48:46 +0300

    make EX_14

---
+++ EX_14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число:");
+string introduced = Console.ReadLine();
+int counter = introduced.Length;
+int size = 3;
+int factor = 1;
+while (size < counter)
+{
+   factor = factor * 10;
+   size++;
+}
+
+
+if (counter < size)
+{
+    Console.WriteLine("Ошибка: Число не состоит из " + size + " цифр");
+}
+else
+{
+    Console.WriteLine(int.Parse(introduced) / factor % 10);
+}
