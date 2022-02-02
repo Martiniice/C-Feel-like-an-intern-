f0f537b03190cc64cd8bd28ff94e8847d70e2c59
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 17:20:30 +0300

    make EX_5

---
+++ EX_5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+
+Console.WriteLine("Введите знаяение а:");
+int a = int.Parse(Console.ReadLine());
+int y = a + 3;
+
+Console.WriteLine(y);
