1cc08ac5cde7d98f29fd97f1790ef3ed807666d1
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 15:16:27 +0300

    make EX_1

---
+++ EX_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+int num1 = new Random().Next(1,10);
+int num2 = new Random().Next(1,82);
+num1 = 2;
+num2 = 4;
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine("num1 : " + num1);
+    Console.WriteLine("num2 : " + num2);
+    Console.WriteLine("True : num1^2 = num2 = " + num2);
+}
+else 
+{
+    Console.WriteLine("num1 : " + num1);
+    Console.WriteLine("num2 : " + num2);
+    Console.WriteLine("False : num1^2 = " + num1 * num1);
+}
