1e8d24e15e8243dd242755e7e725849139a3caf8
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 20:35:59 +0300

    make EX_13

---
+++ EX_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+int num1 = new Random().Next(1,1000);
+Console.WriteLine("Задайте число:");
+int num2 = int.Parse(Console.ReadLine());
+int remainder = num1 % num2;
+Console.WriteLine("Рандомное число = " + num1);
+
+if (remainder == 0)
+{
+    Console.WriteLine("Рандомное число кратно Вашему, Ура!");
+}
+else
+{
+    Console.WriteLine(remainder);
+}
