2bd0db0332e5350d3162f75a0e8070a9c241cc8e
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 19:11:14 +0300

    make EX_9

---
+++ EX_9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+bool resalt1 = num > 99;
+bool resalt2 = num < 1000;
+
+if (resalt1 == resalt2)
+    {
+        Console.WriteLine(num % 10);
+    }
+
+else Console.WriteLine("Ошибка: Введено не ТРЕХЗНАЧНОЕ число!");
