71a5950ee703ce73ffe120a14ec67e43c1ceafa4
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 18:14:24 +0300

    make EX_7

---
+++ EX_7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("Введите min значение N:");
+int minN = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите max значение N:");
+int maxN = int.Parse(Console.ReadLine());
+
+
+int nextN = minN;
+
+while (nextN <= maxN)
+{
+    Console.Write(nextN + ",");
+    nextN++;
+}
+
