6cd87f1aeed576d1d9591c92ff0e11aa01a873cf
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 19:24:42 +0300

    make EX_11

---
+++ EX_11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int num = new Random().Next(10,100);
+int num1 = num / 10;
+int num2 = num % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else 
+{
+    Console.WriteLine(num2);
+}
+
+//Console.WriteLine(num);
