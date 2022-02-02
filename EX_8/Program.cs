e2b0ec1518177271b2eadca673e923f5cde5ba32
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 18:26:04 +0300

    make EX_8

---
+++ EX_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите до какого числа ищем четные числа:");
+int lastNUM  = int.Parse(Console.ReadLine());
+int num = 0;
+
+while (num < lastNUM - 1)
+{
+    num = num + 2;
+    Console.Write(num + ",");
+}
