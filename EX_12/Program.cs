2470e47ad3958e397d0017585707a95eada7a9a0
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 19:31:54 +0300

    make EX_12

---
+++ EX_12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+bool resalt1 = num > 99;
+bool resalt2 = num < 1000;
+
+if (resalt1 == resalt2)
+    {
+        int num1 = num / 100;
+        int num2 = num % 10;
+        num = num1 * 10 + num2;
+        Console.WriteLine(num);
+    }
+
+else Console.WriteLine("Ошибка: Введено не ТРЕХЗНАЧНОЕ число!");
+
