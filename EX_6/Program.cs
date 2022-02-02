358b0f923353fe3207d960fbb180202f8cb4d325
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 17:26:10 +0300

    make EX_6

---
+++ EX_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
