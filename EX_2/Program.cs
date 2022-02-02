84703fc278da4a693ddc81f3c6503b869b4d09c1
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 15:57:02 +0300

    make EX_2

---
+++ EX_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int num1 = new Random().Next(1,100);
+int num2 = new Random().Next(1,100);
+
+if (num1 == num2)
+{
+    Console.WriteLine(num1 + " = " + num2);
+}
+else 
+{
+    if (num1 > num2)
+    {
+        Console.WriteLine("more = " + num1);
+        Console.WriteLine("less = " + num2);
+    }
+    else
+    {
+        Console.WriteLine("more = " + num2);
+        Console.WriteLine("less = " + num1);
+    }
+}
