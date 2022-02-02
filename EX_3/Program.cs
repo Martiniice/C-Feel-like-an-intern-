a011cbc613f6883257b5020c516cc6d88b336a94
MVV <martini.ice.ti.s@yandex.ru>
2022-02-02 16:39:05 +0300

    make EX_3

---
+++ EX_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// По заданному номеру дня недели вывести его название
+string[] array = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+Console.WriteLine("Напишите номер дня: ");
+
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(array[num - 1]);
