8ffbefcd8dfa2f534d78e8972aedcf0594e3e494
MVV <martini.ice.ti.s@yandex.ru>
2022-02-06 17:39:31 +0300

    make EX_20

---
+++ EX_20/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти (от 1 до 4):");
+int quarter = int.Parse(Console.ReadLine());
+
+if (quarter == 1) Console.WriteLine("X: (0;+inf); Y: (0;+inf)");
+else if (quarter == 2) Console.WriteLine("X: (-inf; 0); Y: (0;+inf)"); 
+else if (quarter == 3) Console.WriteLine("X: (-inf; 0); Y: (0;-inf)"); 
+else if (quarter == 3) Console.WriteLine("X: (0;-inf); Y: (0;-inf)"); 
