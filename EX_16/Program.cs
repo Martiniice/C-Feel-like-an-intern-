2f0fd0e9c42f41c0838b009da33ba00b933ef600
MVV <martini.ice.ti.s@yandex.ru>
2022-02-06 17:23:04 +0300

    make EX_16

---
+++ EX_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 0 && num < 8) 
+{
+    if (num < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Ура! Выходной");
+    }
+}
+else Console.WriteLine("В неделе 7 дней!");
