69bdaee1ec67e34751463b58d0fe3c657814f9b1
MVV <martini.ice.ti.s@yandex.ru>
2022-02-06 17:41:44 +0300

    make EX_20

--- EX_20/Program.cs
+++ EX_20/Program.cs
@@ -5,4 +5,4 @@ int quarter = int.Parse(Console.ReadLine());
 if (quarter == 1) Console.WriteLine("X: (0;+inf); Y: (0;+inf)");
 else if (quarter == 2) Console.WriteLine("X: (-inf; 0); Y: (0;+inf)"); 
 else if (quarter == 3) Console.WriteLine("X: (-inf; 0); Y: (0;-inf)"); 
-else if (quarter == 4) Console.WriteLine("X: (0;-inf); Y: (0;-inf)"); 
+else if (quarter == 4) Console.WriteLine("X: (0;inf); Y: (0;-inf)"); 
