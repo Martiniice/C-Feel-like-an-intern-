41fa2293e41bc18a60f014b2cddd3e8d47151e17
MVV <martini.ice.ti.s@yandex.ru>
2022-02-06 17:08:26 +0300

    make EX_18

---
+++ EX_18/Program.cs
@@ -0,0 +1,28 @@
+﻿bool x = true; 
+bool y = false; 
+bool resleft = false; 
+bool resright = false; 
+bool LogNO(bool znach) 
+{ 
+    if (znach == true) znach = false; 
+    else znach = true; 
+    return znach; 
+} 
+bool LogAnd(bool zn1, bool zn2, bool result) 
+{ 
+    if (zn1 && zn2 == true) result = true; 
+    else result = false; 
+    return result; 
+} 
+bool LogOr(bool znach1, bool znach2, bool output) 
+{ 
+    if (znach1 || znach2 == true) output = true; 
+    else output = false; 
+    return output; 
+} 
+LogOr(x, y, resleft); 
+LogNO(resleft); 
+LogNO(x); 
+LogNO(y); 
+LogAnd(x, y, resright); 
+global::System.Console.WriteLine(resleft == resright);
