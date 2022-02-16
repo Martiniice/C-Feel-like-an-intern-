5983f50e5f54280b607d67a6d4d60208d8888f5c
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 18:25:07 +0300

    make EX_28

---
+++ EX_28/Program.cs
@@ -0,0 +1,9 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (sum = 0; number > 0; number /= 10) 
+{
+    sum += number % 10;
+}
+Console.Write("Сумма цифр вашего числа = " + sum);
