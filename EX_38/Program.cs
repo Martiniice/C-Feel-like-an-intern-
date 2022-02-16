904d10a7025d99f2c1b9e5d423e20e5ff62f0b97
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 21:05:07 +0300

    make EX_38

---
+++ EX_38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillArray(int[] numbers)
+{
+    int index = 0;
+    while (index < numbers.Length)
+    {
+        numbers[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] numbs)
+{
+    int index = 0;
+    while (index < numbs.Length)
+    {
+        Console.Write(numbs[index] + " ");
+        index++;
+    }
+}
+void SummNumbersOddIndex(int[] nums)
+{
+    int index = 1;
+    int summ = 0;
+    while (index < nums.Length)
+    {
+        summ += nums[index];
+        index += 2;
+    }
+    System.Console.WriteLine("Сумма чисел стоящих на нечетной позиции равна: " + summ);
+}
+
+int[] array = new int[14];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SummNumbersOddIndex(array);
