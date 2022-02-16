2b8ecc4a7c7b742790c9233b1348018ea31285e0
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 14:47:44 +0300

    make EX_26

---
+++ EX_26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введите число A:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = int.Parse(Console.ReadLine());
+int BOX = 1;
+int i = 0;
+
+while (i < B)
+{
+    BOX = BOX * A;
+    i++;
+}
+Console.WriteLine(A + "^" + B + "=" + BOX);
