cd887f9cc5e41624b089065947a38abfbfea0a51
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 20:00:55 +0300

    make EX_34

---
+++ EX_34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу замену элементов массива на противоположные
+void FillArray(int[] numbers)
+{
+    int index = 0;
+    while (index < numbers.Length)
+    {
+        numbers[index]= new Random().Next(-9,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] nums)
+{
+    int index = 0;
+    while (index < nums.Length)
+    {
+        Console.Write(nums[index] + " ");
+        index++;
+    }
+}
+
+void OppositeArray(int[] numbs)
+{
+    int index = 0;
+    while (index < numbs.Length)
+    {
+        numbs[index] *= -1;
+        index++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+OppositeArray(array);
+PrintArray(array);
