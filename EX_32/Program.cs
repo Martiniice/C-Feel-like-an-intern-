f565c60d5319ed715b504f0aab95804edd50a342
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 19:44:52 +0300

    make EX_32

---
+++ EX_32/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray(int[] numbers)
+{
+    int index = 0;
+    while (index < numbers.Length)
+    {
+        numbers[index]= new Random().Next(0,2);
+        index++;
+    }
+}
+
+void PrintArray(int[] nums)
+{
+    int index = 0;
+    while (index < nums.Length)
+    {
+        System.Console.Write(nums[index]);
+        index++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
