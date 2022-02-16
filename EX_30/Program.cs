3181eb7f72ec236a76f7de4afc7b063a48449b09
MVV <martini.ice.ti.s@yandex.ru>
2022-02-16 19:25:48 +0300

    make EX_30

---
+++ EX_30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Задайте последнее число возводимое в куб: ");
+int number = int.Parse(Console.ReadLine());
+int i = 2;
+while (i < number+1)
+{
+    if ((Math.Pow(i,3)) % 2 == 0)
+    {
+        Console.WriteLine(i + "^3="+ Math.Pow(i,3));
+    }
+    i++;
+}
