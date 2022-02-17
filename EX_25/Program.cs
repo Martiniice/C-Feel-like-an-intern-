426c7f0f1c62eb880b8603f72adb020b55934152
MVV <martini.ice.ti.s@yandex.ru>
2022-02-17 16:28:12 +0300

    make EX_25

---
+++ EX_25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int summ = 0;
+while (num > 0)
+{
+    summ += num;
+    num--;
+}
+Console.WriteLine(summ);
