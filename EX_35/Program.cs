acee4b16d9510ee8e56a66ba62e1e0805bddc4fa
MVV <martini.ice.ti.s@yandex.ru>
2022-02-17 17:34:05 +0300

    make EX_35

---
+++ EX_35/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+void FillArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(0,10);
+}
+void PrintArray (int[]collection)
+{
+    for (int i = 0; i < collection.Length; i++) Console.Write(collection[i] + " ");
+}
+bool CheckingNumberInArray (int[] numbers, int number)
+{
+    bool result = false;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == number)
+        {
+            result = true;
+            break;
+        }
+        else result = false;
+    }
+    return result;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+System.Console.WriteLine(CheckingNumberInArray(array, num));
