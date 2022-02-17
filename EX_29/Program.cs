a99d985a34c3df21f6f462a5b441c02eff929465
MVV <martini.ice.ti.s@yandex.ru>
2022-02-17 16:37:59 +0300

    make EX_29

---
+++ EX_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int total = 1;
+while (num > 1)
+{
+    total *= num;
+    num--;
+}
+Console.WriteLine(total);
