48017b9fce228b2e7e883ba441ccb9eaacd4cc0d
MVV <martini.ice.ti.s@yandex.ru>
2022-02-17 16:49:42 +0300

    make EX_31

---
+++ EX_31/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+void FillArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(1,10);
+}
+void PrintArray (int[]collection)
+{
+    for (int i = 0; i < collection.Length; i++) Console.Write(collection[i] + " ");
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
