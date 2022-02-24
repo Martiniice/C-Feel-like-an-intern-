de1d40a252f5df2a8b342e0f4be6c68b047b4b26
MVV <martini.ice.ti.s@yandex.ru>
2022-02-24 18:15:59 +0300

    make EX_42

---
+++ EX_42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+System.Console.Write("Введите числа через пробел: ");
+string nums = Console.ReadLine();
+var data = nums.Split(" ")
+            .Select(item => item.Split(" "))
+            .Select(e => int.Parse(e[0]))         
+            .ToArray();
+int count = 0;
+for (int i = 0; i < data.Length; i++)
+{
+    if (data[i] > 0) count++;
+}
+System.Console.WriteLine($"Введено {count} чисел больше 0");
