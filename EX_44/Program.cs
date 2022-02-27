f98e18e9716bc3ac3049550ff8cc7d3e33b3e151
MVV <martini.ice.ti.s@yandex.ru>
2022-02-27 21:55:47 +0300

    fixed EX_44

--- EX_44/Program.cs
+++ EX_44/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы ответ (x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1)
 
-System.Console.WriteLine("уравнение двух прямых ");
+System.Console.WriteLine("уравнение двух прямых y = k1 * x + b1, y = k2 * x + b2");
+System.Console.WriteLine("Введите k1:");
+int k1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите b1:");
+int b1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите k2:");
+int k2 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите b2:");
+int b2 = int.Parse(Console.ReadLine());
+double X = (b2-b1)/(k1-k2);
+double Y = k1*(b2-b1)/((k1-k2)+b1);
+
+System.Console.WriteLine($"Точка пересечения X:{X} Y:{Y}");
