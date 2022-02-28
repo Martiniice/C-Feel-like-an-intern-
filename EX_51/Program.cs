6a7d95f8c28b5f2aaf9080161a52791ec7de8108
MVV <martini.ice.ti.s@yandex.ru>
2022-02-28 13:25:59 +0300

    make EX_51

---
+++ EX_51/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) 
+        {
+            arr[i, j] = i + j;
+        }    
+    }
+}
+void PrintArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++) 
+        {
+            Console.Write(collection[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] array = new int[5, 4];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
